3dacb5b0895f123aaa2824d15c262597276e9425
daniellzl <mr.daniel.liang@gmail.com>
2017-08-14 23:43:47 -0700

    fixed app to be compatible with changes made to Yahoo API

--- app.js
+++ app.js
@@ -15,15 +15,14 @@ var model = require('./models/models');
 var app = express();
 
 // mongo database
-// var connectionString = process.env.MONGO_DB;
-var connectionString = 'mongodb://user:asdf@ds035816.mlab.com:35816/stock_market_app';
+var connectionString = process.env.MONGO_DB;
 mongoose.connect(connectionString);
 var db = mongoose.connection;
-db.on('error', function(){
-    console.log('There was an error connecting to the database');
+db.on('error', function() {
+  console.log('There was an error connecting to the database');
 });
 db.once('open', function() {
-    console.log('Successfully connected to database');
+  console.log('Successfully connected to database');
 });
 
 // create http reference to app
@@ -34,12 +33,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // http request logger
-app.use(logger("dev", { format: 'dev', immediate: true }));
+app.use(logger("dev", {
+  format: 'dev',
+  immediate: true
+}));
 
 // templating engine
-var hbs = handlebars.create({
-    defaultLayout: 'main'
-});
+var hbs = handlebars.create({defaultLayout: 'main'});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -50,234 +50,212 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
 // home page
-app.get('/', function(req,res) {
-    return res.render('home/index');
+app.get('/', function(req, res) {
+  return res.render('home/index');
 });
 
 // listen on connection event for incoming sockets
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 
 /******
 Display
 ******/
 
-    // gather information
-    socket.on('gatherInfo', function(view) {
-        
-        // query database for stocks
-        model.stockModel.find({}, function(err, dbResults) {
-            if (err) throw err;
-            
-            // if database is empty
-            if (dbResults.length === 0) {
-                return socket.emit('emptyDatabase');
-            } 
-            
-            // extract stock symbols from database
-            var symbols = [];
-            for (var i = 0, l = dbResults.length; i < l; i++) {
-                symbols.push(dbResults[i].symbol);
-                yahooFinance.quote({
-                	symbol: dbResults[i].symbol,
-                	modules: [ 'price' ]
-                }, function (err, quotes) {
-        			if (err) throw err;
+// gather information
+socket.on('gatherInfo', function(view) {
 
-        			snapshot = {
-        				symbol: quotes.price.symbol,
-        				name: quotes.price.longName,
-        				lastTradeDate: dateFormat(quotes.price.regularMarketTime, 'm/d/yy'),
-        				lastTradePriceOnly: quotes.price.regularMarketPreviousClose
-        			}
+  // query database for stocks
+  model.stockModel.find({}, function(err, dbResults) {
+    if (err) throw err;
 
-        			console.log(snapshot);
-        			
-		            // tell client browser to display buttons
-		            socket.emit('displayButton', snapshot);
-        		});
+    // if database is empty
+    if (dbResults.length === 0) {
+      return socket.emit('emptyDatabase');
+    }
+
+    // extract stock symbols from database
+    var symbols = [];
+    for (var i = 0, l = dbResults.length; i < l; i++) {
+      // save symbols for
+      symbols.push(dbResults[i].symbol);
+    }
+
+    // look up current stock info with yahoo finance
+    for (var i = 0, l = dbResults.length; i < l; i++) {
+      yahooFinance.quote({
+        symbol: dbResults[i].symbol,
+        modules: ['price']
+      }, function(err, quotes) {
+        if (err) throw err;
+
+        // create snapshot of stock information
+        snapshot = {
+          symbol: quotes.price.symbol,
+          name: quotes.price.longName,
+          lastTradeDate: dateFormat(quotes.price.regularMarketTime, 'm/d/yy'),
+          lastTradePriceOnly: quotes.price.regularMarketPrice
+        }
+
+        // tell client browser to display buttons
+        socket.emit('displayButton', snapshot);
+      });
+    }
+
+    // set date range to be queried
+    var from = new Date();
+    switch (view) {
+      case '1w':
+        from.setDate(from.getDate() - 7);
+        break;
+      case '1m':
+        from.setMonth(from.getMonth() - 1);
+        break;
+      case '3m':
+        from.setMonth(from.getMonth() - 3);
+        break;
+      case '6m':
+        from.setMonth(from.getMonth() - 6);
+        break;
+      case '1y':
+        from.setFullYear(from.getFullYear() - 1);
+        break;
+      case '3y':
+        from.setFullYear(from.getFullYear() - 3);
+        break;
+      default:
+        from.setDate(from.getDate() - 7);
+    }
+
+    // query Yahoo Finance for historical info
+    yahooFinance.historical({
+      symbols: symbols,
+      from: from,
+      to: new Date
+    }, function(err, result) {
+      if (err) throw err;
+
+      // object to store return information
+      var info = {
+        dates: [],
+        prices: []
+      };
+
+      // loop through results and save dates and prices to return object
+      var bool = true;
+      for (var key in result) {
+
+        var object = {
+          name: key,
+          data: []
+        };
+
+        // if more than 15 stock prices gathered
+        if (result[key].length >= 10) {
+          for (var i = 0, l = result[key].length; i < l; i += Math.floor(l / 10)) {
+            object.data.unshift(parseFloat(result[key][i].open.toFixed(2)));
+            if (bool) {
+              var d = new Date(result[key][i].date);
+              info.dates.unshift(dateFormat(d, 'm/d/yy'));
             }
+          }
 
-
-            
-            // // query yahoo finance for stock info of the day
-            // yahooFinance.snapshot({
-            //     symbols: symbols,
-            //     fields: ['s', 'n', 'd1', 'l1']
-            // }, function (err, snapshot) {
-            //     if (err) throw err;
-                
-            //     // replace timestamps with dates
-            //     for (var i = 0, l = snapshot.length; i < l; i++) {
-            //         snapshot[i].lastTradeDate = dateFormat(snapshot[i].lastTradeDate, 'm/d/yy');
-            //     }
-                
-            //     // tell client browser to display buttons
-            //     socket.emit('displayButtons', snapshot);
-            // });
-            
-            // set date range to be queried
-            var from = new Date();
-            switch(view) {
-                case '1w':
-                    from.setDate(from.getDate() - 7);
-                    break;
-                case '1m':
-                    from.setMonth(from.getMonth() - 1);
-                    break;
-                case '3m':
-                    from.setMonth(from.getMonth() - 3);
-                    break;
-                case '6m':
-                    from.setMonth(from.getMonth() - 6);
-                    break;
-                case '1y':
-                    from.setFullYear(from.getFullYear() - 1);
-                    break;
-                case '3y':
-                    from.setFullYear(from.getFullYear() -3);
-                    break;
-                default:
-                    from.setDate(from.getDate() - 7);
+          // if less than 15 stock prices gathered
+        } else {
+          for (var i = 0, l = result[key].length; i < l; i++) {
+            object.data.unshift(parseFloat(result[key][i].open.toFixed(2)));
+            if (bool) {
+              var d = new Date(result[key][i].date);
+              info.dates.unshift(dateFormat(d, 'm/d/yy'));
             }
-            
-            // query Yahoo Finance for historical info
-            yahooFinance.historical({
-                symbols: symbols,
-                from: from,
-                to: new Date
-            }, function (err, result) {
-                if (err) throw err;
-                
-                // object to store return information
-                var info = {
-                    dates: [],
-                    prices: []
-                };
-                
-                // loop through results and save dates and prices to return object
-                var bool = true;
-                for (var key in result) {
-                    
-                    var object = {
-                        name: key,
-                        data: []
-                    };
-                    
-                    // if more than 15 stock prices gathered
-                    if (result[key].length >= 10) {
-                        for (var i = 0, l = result[key].length; i < l; i += Math.floor(l/10)) {
-                            object.data.push(parseFloat(result[key][i].open.toFixed(2)));
-                            if (bool) {
-                                var d = new Date(result[key][i].date);
-                                info.dates.push(dateFormat(d, 'm/d/yy'));
-                            }
-                        }
-                        
-                        
-                    // if less than 15 stock prices gathered
-                    } else {
-                        for (var i = 0, l = result[key].length; i < l; i ++) {
-                            object.data.push(parseFloat(result[key][i].open.toFixed(2)));
-                            if (bool) {
-                                var d = new Date(result[key][i].date);
-                                info.dates.push(dateFormat(d, 'm/d/yy'));
-                            }
-                        }
-                    }
-                    
-                    // save to info object
-                    info.prices.push(object);
-                    bool = false;
-                }
-                
-                // tell client browser to display graph
-                return socket.emit('displayGraph', info);
-            });
-        });
+          }
+        }
+
+        // save to info object
+        info.prices.push(object);
+        bool = false;
+      }
+
+      // tell client browser to display graph
+      return socket.emit('displayGraph', info);
     });
+  });
+});
 
 /********
 Add Stock
 ********/
-    
-    // add stock
-    socket.on('addStock', function(stock) {
-        
-        // capitalize stock symbol
-        stock = stock.toUpperCase();
-        
-        // if empty notify
-        if (stock === '') {
-            return socket.emit('stockDoesntExist');
-        }
-        
-        // check if stock name exists
-        yahooFinance.snapshot({
-            symbol: stock,
-            fields: ['n'],
-        }, function (err, snapshot) {
-            if (err) throw err;
-            
-            // stock doesn't exist
-            if (snapshot.name === null) {
-                
-                // alert only sender of error
-                return socket.emit('stockDoesntExist');
-                
-            // stock exists
-            } else {
-                
-                // check if stock is already in database
-                model.stockModel.findOne({ symbol: stock }, function(err, dbResults) {
-                    if (err) throw err;
-                    
-                    // if not found add to database
-                    if (dbResults === null) {
-                        
-                        // save stock to database
-                        var s = model.stockModel({
-                            symbol: stock
-                        });
-                        s.save(function(err){
-                            if (err) throw err;
-                            
-                            // broadcast to all users
-                            return io.emit('stockAdded');
-                        });
-                        
-                    } else {
-                        
-                        // alert sender stock already exists
-                        return socket.emit('stockNotAdded');
-                    }
-                });
-            }
-        });
-    });
 
-/***********
+// add stock
+socket.on('addStock', function(stock) {
+
+  // capitalize stock symbol
+  stock = stock.toUpperCase();
+
+  // if empty notify
+  if (stock === '') {
+    return socket.emit('stockDoesntExist');
+  }
+
+  // check if stock name exists
+  yahooFinance.quote({
+    symbol: stock,
+    fields: ['price']
+  }, function(err, snapshot) {
+    // stock doesn't exist
+    if (err) return socket.emit('stockDoesntExist');
+
+    // check if stock is already in database
+    model.stockModel.findOne({
+      symbol: stock
+    }, function(err, dbResults) {
+      if (err) throw err;
+
+      // if not found add to database
+      if (dbResults === null) {
+
+        // save stock to database
+        var s = model.stockModel({symbol: stock});
+        s.save(function(err) {
+          if (err) throw err;
+
+          // broadcast to all users
+          return io.emit('stockAdded');
+        });
+
+      } else {
+
+        // alert sender stock already exists
+        return socket.emit('stockNotAdded');
+      }
+    });
+  });
+});
+
+  /***********
 Remove Stock
 ***********/
-    
-    // remove stock
-    socket.on('removeStock', function(stock) {
-        
-        // capitalize stock symbol
-        stock = stock.toUpperCase();
-        
-        // remove stock in database
-        model.stockModel.findOneAndRemove({ symbol: stock }, function(err) {
-		    if (err) throw err;
-		    
-		    // notify all users
-		    return io.emit('stockRemoved');
-		});
-    });
-    
+
+// remove stock
+socket.on('removeStock', function(stock) {
+
+  // capitalize stock symbol
+  stock = stock.toUpperCase();
+
+  // remove stock in database
+  model.stockModel.findOneAndRemove({
+    symbol: stock
+  }, function(err) {
+    if (err)throw err;
+
+    // notify all users
+    return io.emit('stockRemoved');
+  });
+});
+
 });
 
 // listen for connections
 var port = process.env.PORT || 8080;
 http.listen(port, function() {
-    console.log('Listening on port ' + port);
+  console.log('Listening on port ' + port);
 });
